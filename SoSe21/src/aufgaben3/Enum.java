2721c87354d59501e625beb5d232734370b7e259
Boghiey <82495261+Boghiey@users.noreply.github.com>
2021-06-06 23:53:19 +0200

    Aufgabe 3

---
+++ SoSe21/src/aufgaben3/Enum.java
@@ -0,0 +1,11 @@
+package aufgaben3;
+
+/*
+ * FREE - der Platz ist ein Spielfeld, aber kein Spielstein drauf
+ * USED - der Platz ist ein Spielfeld mit Spielstein drauf
+ * NOT  - der Platz gehoert nicht zum Spielfeld
+ */
+
+public class Enum {
+	 FREE, USED, NOT
+}
