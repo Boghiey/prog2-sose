ce02e4f2c83ea43ab32477978238768b67cc82ae
Boghiey <82495261+Boghiey@users.noreply.github.com>
2021-06-06 23:55:45 +0200

    Aufgabe 6

---
+++ SoSe21/src/aufgabe6/PizzaMenu.java
@@ -0,0 +1,39 @@
+package aufgabe6;
+
+import java.util.*;
+
+public class PizzaMenu {
+	
+	public PizzaMenu() {
+	Map<String, Integer> mapB= new HashMap<>();
+	mapB.put("Margherita", 700);
+	mapB.put("Veggie", 750);
+	mapB.put("Salami", 850);
+	mapB.put("Schinken", 850);
+	
+	}
+	public static boolean existingPizza(Map<String,Double> menu, String pizza)
+	{
+		// check if entered pizza is found on the menu
+		/*Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string: ");
+		String str=sc.nextLine();    // read string
+		*/
+		
+		System.out.println();
+		 for(Map.Entry<String, Integer> entry : mapB.entrySet())
+		 {
+			 
+		 }
+		
+}
+	public static void printPrice(Map<String,Double> menu, String pizza)
+	{
+		// gives out the price of the entered pizza
+	}
+	
+	public printMenu()
+	{
+		
+	}
+}
---
+++ SoSe21/src/aufgabe6/Testcode.java
@@ -0,0 +1,19 @@
+package aufgabe6;
+
+public class Testcode {
+
+	public static void main(String[] args) {
+		// 1
+		long a = 1000;
+		Integer b = 1000;       
+		Integer c = 1000;       
+		System.out.println(a == b);
+		System.out.println(b == c);
+		
+		/*beim erste laufen des program stelle ich fest dass,
+		 *a==b ist ture und b==c ist false
+		 *Der vergleich der Variable bleibt immer false und ändern sich nicht 
+		 */
+	}
+
+}
